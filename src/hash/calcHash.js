f35e19db22bf5d9eedfd653e77668e0d0aa93fc7
Rahmiddin Asiydinov <81851084+rahmiddinasiydinov@users.noreply.github.com>
2023-06-20 00:50:00 +0500

    Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import crypto from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = './files/fileToCalculateHashFor.txt';
+    const hash = crypto.createHash('sha256');
+    const input = fs.createReadStream(fileName);
+    input.on('data', (chunk) => {
+        hash.update(chunk);
+      });
+    
+      input.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log('SHA256 hash:', fileHash);
+      });
+    
+      input.on('error', (error) => {
+        console.error('Error calculating hash:', error);
+      });
+    
 };
 
 await calculateHash();
